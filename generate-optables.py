3dffa1310cb1483351404ec6026d4d41a6ee42fe
Michael Barrientos <michaelbarrientos788@gmail.com>
2026-06-20 11:00:05 -0400

    Generating the header file for the optables

--- generate-optables.py
+++ generate-optables.py
@@ -1,8 +1,11 @@
 #!/usr/bin/env python3
 import json
 
-def generate_optable(prefixed_optable_data):
-    for opcode, data in prefixed_optable_data.items(): 
+def generate_optable(optable_data, prefix):
+    # Write the array signature.
+    optable_buf = f"static const struct opcode_info {prefix}[256] = {{"
+
+    for opcode, data in optable_data[prefix].items(): 
         # Form the mnemonic
         mnemonic = data["mnemonic"]
         for operand in data["operands"]:
@@ -17,14 +20,29 @@ def generate_optable(prefixed_optable_data):
                     len(data["cycles"]) == 1 else data["cycles"][1]}"
 
         
-        print(f"{{ {opcode}, {cycles}, \"{mnemonic}\" }},")
+        # Append the entry
+        optable_buf += f"\n  {{ {opcode}, {cycles}, \"{mnemonic}\" }},"
+
+    # Remove trailing comma at the last entry, and close the array
+    return f"{optable_buf[:-1]}}};\n\n"
 
 def main():
     with open("Opcodes.json", "r") as optable_json:
         optable_data = json.load(optable_json)
-        generate_optable(optable_data["unprefixed"])
-        print("")
-        generate_optable(optable_data["cbprefixed"])
+        with open("optables.h", "w") as output_file:
+            output_file.write(
+                "#pragma once\n"
+                "#include <stdint.h>\n\n"
+                "struct opcode_info {\n"
+                "  uint8_t opcode;\n"
+                "  uint8_t cycles_taken;\n"
+                "  uint8_t cycles_untaken;\n"
+                "  const char *mnemonic;\n"
+                "};\n\n"
+            )
+            output_file.write(generate_optable(optable_data, "unprefixed"))
+            output_file.write("")
+            output_file.write(generate_optable(optable_data, "cbprefixed"))
 
 if __name__ == "__main__":
     main()
